df487f99460fa83ffa8d47a1625de73472082721
Ricardo Osit <aizekrysler@gmail.com>
2017-01-08 18:32:25 +0800

    another test
    another test

--- test.asm
+++ test.asm
@@ -7,7 +7,7 @@ STATUS EQU 03H
 CLRW
 
 BSF STATUS,5
-MOVLW B'0000010';
+MOVLW B'0010000' ;test input
 MOVWF TRISA
 MOVLW B'0000000'
 MOVWF TRISB
@@ -16,11 +16,11 @@ BCF STATUS,5
 CLRF PORTA
 
 START
-MOVLW B'0001100'
+MOVLW B'0000010' ;actual input
 MOVWF PORTA
 
 
-BTFSC PORTA,2 ;check kung naka zero ung bit1 ng portA
+BTFSC PORTA,1 ;check kung naka zero ung bit1 ng portA
 GOTO EIGHT ;kapag naka 1 ung bit1 ng portA execute EIGHT routine [means from bit6 to bit12 naka 1] 
 GOTO ZERO ;kapag naka 0 ung bit1 ng portA execute ZERO routine [means from bit6 to bit11 naka 1 tpos yung bit12 naka 0]
 
